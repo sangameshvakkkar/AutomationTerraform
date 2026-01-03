541074ccc67c9a9422b3e0bead127c4b385aeed1
Sangamesh Vakkar <sangameshvakkar7424@gmail.com>
2026-01-04 00:38:12 +0530

    Full CI/CD for Terraform on AWS

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "suraj-terraform-state-demo"
+    bucket         = "aura-cicd-terraform-state"
     key            = "cicd/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
