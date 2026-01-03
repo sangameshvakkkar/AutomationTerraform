5eb10a5e29dd4da1962a88e0258d481519aab13f
Sangamesh Vakkar <sangameshvakkar7424@gmail.com>
2026-01-04 01:06:40 +0530

    Remove deprecated dynamodb_table and use lockfile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "aura-cicd-terraform-state"
-    key            = "cicd/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "aura-cicd-terraform-state"
+    key          = "cicd/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
