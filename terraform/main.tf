6ea77cc4dce7c531bcbc3ad4f799c0d6fc44e212
Sangamesh Vakkar <sangameshvakkar7424@gmail.com>
2026-01-04 13:15:24 +0530

    Protect resource with prevent_destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,13 @@
 resource "aws_s3_bucket" "demo" {
   bucket = var.bucket_name
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
   tags = {
     Project = "Aura-Terraform-CICD"
     Owner   = "suraj"
   }
 }
 
-###
