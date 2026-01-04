23aa7ebdc20c9e96a6ba8c0e7f449e2dd280f04e
Sangamesh Vakkar <sangameshvakkar7424@gmail.com>
2026-01-04 19:13:35 +0530

    feat: add common variable definitions for `bucket_name`, `environment`, and `tags` to dev and prod environments.

--- envs/dev/vars.tf
+++ envs/dev/vars.tf
@@ -1,15 +1,4 @@
-## backend.terraform 
-variable "backend_bucket_name" {
-  type = string
-}
 
-variable "backend_bucket_key" {
-  type = string
-}
-
-variable "backend_region" {
-  type = string
-}
 
 variable "bucket_name" {
   type = string
